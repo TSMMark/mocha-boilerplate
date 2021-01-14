ac04481d5fbb3851dcc2cc446bac6fa0f7ab503d
TSMMark <mark.allen@vydia.com>
2021-01-14 16:05:04 -0500

    add package.json and example index.js

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const Mocha = require('mocha')
+const mocha = new Mocha()
+const { assert } = require('chai');
+mocha.suite.emit('pre-require', this, 'solution', mocha)
+const { describe, it } = Mocha
+
+
+//================================================================================
+// Your Code
+//================================================================================
+
+
+// Return a number that is twice the value of the argument
+const doubler = (n) => n
+
+
+//================================================================================
+// Your Tests
+//================================================================================
+
+
+it('returns 0 when given 0', () => {
+  assert.strictEqual(doubler(0), 0)
+})
+
+// Can you fix the `doubler` function so that this tests passes?
+it('returns 10 when given 5', () => {
+  assert.strictEqual(doubler(5), 10)
+})
+
+
+//================================================================================
+// Boilerplate
+//================================================================================
+
+
+mocha.run()
