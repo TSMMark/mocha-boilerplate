584960a54e96dedf9481f910f0be2e1ab946a90d
TSMMark <mark.allen@vydia.com>
2021-01-15 11:43:30 -0500

    phone number anagrams

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
 const Mocha = require('mocha')
 const mocha = new Mocha()
 const { assert } = require('chai');
-const { strictEqual } = assert
+const { deepEqual } = assert
 mocha.suite.emit('pre-require', this, 'solution', mocha)
 const { describe, it } = Mocha
 
+/* ------------------------------------------------------------------------- */
 
-//================================================================================
-// Your Code
-//================================================================================
+const phoneAnagram = (digits) => {
+  const digitLetterMap = {
+    '0': [' '],
+    '1': ['1'],
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+  }
 
+  return digits.split('').reduce((anagrams, digit) => {
+    const chars = digitLetterMap[digit]
+    return anagrams.length ? anagrams.flatMap((p) => chars.flatMap((char) => [`${p}${char}`])) : chars
+  }, [])
+}
 
-// Return a number that is twice the value of the argument
-const doubler = (n) => n
+/* ------------------------------------------------------------------------- */
 
-
-//================================================================================
-// Your Tests
-//================================================================================
-
-
-it('returns 0 when given 0', () => {
-  strictEqual(doubler(0), 0)
+it('returns empty array when givin no digits', () => {
+  const result = phoneAnagram('')
+  deepEqual(result, [])
 })
 
-// Can you fix the `doubler` function so that this tests passes?
-it('returns 10 when given 5', () => {
-  strictEqual(doubler(5), 10)
+it('returns expected results for 1 digit', () => {
+  const result = phoneAnagram('2')
+  deepEqual(result, ['a', 'b', 'c'])
 })
 
+it('returns expected results for 2 digits', () => {
+  const result = phoneAnagram('23')
+  deepEqual(result, ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+})
 
-//================================================================================
-// Boilerplate
-//================================================================================
+it('returns expected results for 2 digits', () => {
+  const result = phoneAnagram('589')
+  deepEqual(result, ['jtw', 'jtx', 'jty', 'jtz', 'juw', 'jux', 'juy', 'juz', 'jvw', 'jvx', 'jvy', 'jvz', 'ktw', 'ktx', 'kty', 'ktz', 'kuw', 'kux', 'kuy', 'kuz', 'kvw', 'kvx', 'kvy', 'kvz', 'ltw', 'ltx', 'lty', 'ltz', 'luw', 'lux', 'luy', 'luz', 'lvw', 'lvx', 'lvy', 'lvz'])
+})
 
+/* ------------------------------------------------------------------------- */
 
 mocha.run()
